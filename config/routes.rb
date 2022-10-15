7d197228cc7f6e6267916efefbbe10e331a8848c
Felippe Oka <felippe.oka@gmail.com>
2022-10-15 16:06:55 -0300

    WL

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :lists do
-    resources :bookmarks
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: :destroy
 end
